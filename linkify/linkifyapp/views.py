be2a6fa293379503bac846338b6e5385ddd26484
booperxd <spamisfun420@gmail.com>
2023-04-19 18:35:31 -0400

    automatically add new user to database

--- linkify/linkifyapp/views.py
+++ linkify/linkifyapp/views.py
@@ -22,10 +22,20 @@ def login_test(request):
     cache_handler = spotipy.DjangoSessionCacheHandler(request)
     auth_manager = spotipy.oauth2.SpotifyOAuth(client_id = client_id, client_secret=client_secret,redirect_uri=redirect_uri, scope = 'user-read-currently-playing user-modify-playback-state user-read-playback-state', cache_handler=cache_handler)
     if auth_manager.validate_token(cache_handler.get_cached_token()):
+        sp = spotipy.Spotify(cache_handler.get_cached_token()['access_token'])
+        check_if_user_exists(sp)
         return cache_handler.get_cached_token()
     cache_handler.save_token_to_cache(auth_manager.get_access_token())
+    sp = spotipy.Spotify(cache_handler.get_cached_token()['access_token'])
+    check_if_user_exists(sp)
     return cache_handler.get_cached_token()
 
+def check_if_user_exists(sp):
+    id= sp.me()['id']
+    name = sp.me()['display_name']
+    if not User.objects.filter(id=id,username=name).exists():
+        u = User(id=id,username=name)
+        u.save()
 
 def get_current_song(request):
     login_test(request)
